0bed86d5c0dec6b1fcf2ac26bd729f81ca3502f1
arijitbose <arijitbos@gmail.com>
2022-10-14 19:48:31 +0530

    Web App Working

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import json
 import pickle
-import sklearn.utils._bunch
+
 import joblib
 from flask import Flask,request,app,jsonify,url_for,render_template
+from ipynb.fs.full.spaceship_titanic_experiment import ExperimentalTransformer
+import json
 import numpy as np
 import pandas as pd
+from pandas import json_normalize
 
 app=Flask(__name__)
 ## Load the model
 
-model=pickle.load(f)
-scalar=pickle.load(open('preprocessing_for_test.pkl','rb'))
+model=joblib.load('model.sav')
+scalar=joblib.load('preprocessing.sav')
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -19,18 +22,25 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=model.predict(new_data)
-    print(output)
-    return jsonify(output)
+    df2=json_normalize(data)
+    print(df2)
+    # print(obj3.transform(df2))
+    # print(np.array(list(data.values())).reshape(1,-1))
+    
+    output = model.predict(scalar.transform(df2))
+    # output=model.predict(new_data)
+    print(output[0])
+    return str(output[0])
+    
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output=model.predict(final_input)[0]
+    output=model.predict(final_input)
+    print(output)
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
