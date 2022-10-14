c13c8ba9eb2af39dbc7c5ecb2f6727225d8104d2
arijitbose <arijitbos@gmail.com>
2022-10-14 11:13:29 +0530

    Using JobLib

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import json
 import pickle
-
+import sklearn.utils._bunch
+import joblib
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-model=pickle.load(open('model.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+
+model=pickle.load(f)
+scalar=pickle.load(open('preprocessing_for_test.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -20,8 +22,8 @@ def predict_api():
     print(np.array(list(data.values())).reshape(1,-1))
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    print(output)
+    return jsonify(output)
 
 @app.route('/predict',methods=['POST'])
 def predict():
