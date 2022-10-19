379800d853adfd631b7a7b701720960e4f0c76a0
arijitbose <arijitbos@gmail.com>
2022-10-19 15:20:53 +0530

    Web App working

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="Fire Prediction {}".format(output))
-
-
+    return render_template("home.html",prediction_text="Prediction Algerian Forest Fire is {}".format(output))
+   
+    # return data
 
 if __name__=="__main__":
     app.run(debug=True)
